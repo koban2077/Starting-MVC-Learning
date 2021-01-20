d1576279b069921b4304fe5a5b447d37bc8655e4
Aleksei Marakhin <aleksandr671@yandex.ua>
2021-01-20 15:35:11 +0200

    added composer x2

--- public/index.php
+++ public/index.php
@@ -8,16 +8,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require 'autoload.php';
-
-$autoloader = new Psr4AutoloaderClass();
-$autoloader->addNamespace('App', __DIR__ . '/../src');
-$autoloader->register();
-
-//Config::get('DBUSER');
-//exit;
-
-require __DIR__ . '/../helpers.php';
+require __DIR__ . '/../vendor/autoload.php';
+//require __DIR__ . '/../helpers.php';
 
 $router = new Router();
 $router->process();
